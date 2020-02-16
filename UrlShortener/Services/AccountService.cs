6a4f291db3a260dd774ab4088662e88b37d8523e
dejo17 <dean.juricic@gmail.com>
2020-02-16 12:13:42 +0100

    Extract DB context from AccountController to AccountService

--- UrlShortener/Services/AccountService.cs
+++ UrlShortener/Services/AccountService.cs
@@ -10,7 +10,8 @@ namespace UrlShortener.Services
 
     public interface IAccountService {
         Task<Account> Authenticate(string username, string password);
-        Task<IEnumerable<Account>> GetAll(); //TODO napravit reviziju ovoga kasnije
+        Task<Account> GetAccount(string AccountID);
+        Task<Account> CreateAccount(string AccountID);
     }
     public class AccountService : IAccountService
     {
@@ -21,25 +22,59 @@ namespace UrlShortener.Services
         }
 
 
-        public async Task<Account> Authenticate(string username, string password)
+        public async Task<Account> Authenticate(string AccountID, string password)
         {
-            Console.WriteLine("About to authenticate user");
-            Account accountToAuthenticate = await _context.Accounts.FindAsync(username);
+            Console.WriteLine("About to authenticate account");
+            Account accountToAuthenticate = await _context.Accounts.FindAsync(AccountID);
             if (accountToAuthenticate == null)
             {
-                Console.WriteLine("User not found");
+                Console.WriteLine("Account not found");
                 return null; //varacamo null ako account ne postoji
             }
             else {
                 //ako je user pronaden, provjeravamo password
-                Console.WriteLine("User found, checking password");
+                Console.WriteLine("Account found, checking password");
                 return (accountToAuthenticate.Password.Equals(password, StringComparison.Ordinal)) ? accountToAuthenticate : null;
             }
         }
 
-        public Task<IEnumerable<Account>> GetAll()
+        /**
+         * 
+         */
+        public async Task<Account> GetAccount(string AccountID)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Searching for AccountID {AccountID}");
+            Account accountFound = await _context.Accounts.FindAsync(AccountID);
+            if (accountFound == null)
+            {
+                Console.WriteLine("Account not found");
+                return null; //varacamo null ako account ne postoji
+            }
+            else
+            {
+                //ako je user pronaden, provjeravamo password
+                Console.WriteLine("Account found");
+                return accountFound;
+            }
         }
+        public async Task<Account> CreateAccount(string AccountID) {
+
+            Account account = await GetAccount(AccountID);
+            if (account != null)
+            {
+                //account vec postoji, vracamo ga
+                return account;
+            }
+            else {
+                //account ne postoji, kreiramo novi i spremamo u bazu:
+                Account newAccount = new Account();
+                newAccount.AccountID = AccountID;
+                newAccount.Password = "365dfbdr";    //TODO make random password generator
+                _context.Accounts.Add(newAccount);
+                int z = _context.SaveChanges();
+                return newAccount;
+            }
+        }
+
     }
 }
