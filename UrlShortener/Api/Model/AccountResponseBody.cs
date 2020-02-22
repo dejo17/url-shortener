cd601ccc30b4cc553cf3a521b7ed66355d78d286
dejo17 <dean.juricic@gmail.com>
2020-02-22 09:07:09 +0100

    Remove uneccessary comments

--- UrlShortener/Api/Model/AccountResponseBody.cs
+++ UrlShortener/Api/Model/AccountResponseBody.cs
@@ -8,15 +8,8 @@ namespace UrlShortener
 {
     public class AccountResponseBody
     {
-#pragma warning disable IDE1006 // Naming Styles
         public bool success { get; set; }
-#pragma warning restore IDE1006 // Naming Styles
-#pragma warning disable IDE1006 // Naming Styles
         public string description { get; set; }
-#pragma warning restore IDE1006 // Naming Styles
-#pragma warning disable IDE1006 // Naming Styles
         public string password { get; set; }
-#pragma warning restore IDE1006 // Naming Styles
-
     }
 }
