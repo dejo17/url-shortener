5ab925632ee56a09cd2502194aa6c92c809702bb
dejo17 <dean.juricic@gmail.com>
2020-02-22 09:00:08 +0100

    Add RegisterController unit tests

--- UrlShortener.Tests/Controllers/RegisterControllerTest.cs
+++ UrlShortener.Tests/Controllers/RegisterControllerTest.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Text;
+using UrlShortener.Api.Model;
 using UrlShortener.Controllers;
 using UrlShortener.Domain.Models;
 using UrlShortener.Services;
@@ -17,22 +18,42 @@ namespace UrlShortener.Tests.Controllers
     public class RegisterControllerTest
     {
         private readonly Mock<IShortenerService> _mockService;
-        private RegisterController _controller;
+        private UrlController _controller;
 
 
         public RegisterControllerTest()
         {
             _mockService = new Mock<IShortenerService>();
-            InitializeUnauthorizedController();
+            InitializeControllerWithUnauthorizedContext();
         }
 
+        [Fact]
+        public void RegiterUrl_OkResult()
+        {
+            InitializeControllerWithAuthorizedContext();
+            _mockService.Setup(service => service.CreateRegisteredUrl(It.IsAny<RegisterUrlRequestBody>(), It.IsAny<string>()))
+             .Returns(new RegisteredUrl() { LongUrl = "www.google.com", ShortUrl = "AStzHGv", AccountID = "54321" });
+
+            RegisterUrlRequestBody request = new RegisterUrlRequestBody() { url = "www.google.com", redirectType = 301 };
+
+            var actionResult = _controller.RegisterUrl(request);
+            Assert.IsType<ActionResult<RegisterUrlResponseBody>>(actionResult);
+
+            OkObjectResult okResult = (OkObjectResult)actionResult.Result;
+            Assert.IsType<OkObjectResult>((OkObjectResult)actionResult.Result);
+
+            Assert.IsType<RegisterUrlResponseBody>(okResult.Value);
+
+            RegisterUrlResponseBody response = (RegisterUrlResponseBody)okResult.Value;
+            Assert.Equal("AStzHGv", response.shortUrl);
+
+        }
 
         [Fact]
         public void RegiterUrl_ModelNotValid()
         {
+            InitializeControllerWithAuthorizedContext();
 
-            /* _mockService.Setup(service => service.CreateRegisteredUrl(It.IsAny<RegisterUrlRequestBody>(), It.IsAny<string>()))
-             .Returns(new RegisteredUrl() { LongUrl = "www.google.com", ShortUrl = "AStzHGv", AccountID = "12345" });*/
             _mockService.Setup(service => service.CreateRegisteredUrl(It.IsAny<RegisterUrlRequestBody>(), It.IsAny<string>()))
             .Returns((RegisteredUrl)null);
 
@@ -48,9 +69,8 @@ namespace UrlShortener.Tests.Controllers
         {
 
              _mockService.Setup(service => service.CreateRegisteredUrl(It.IsAny<RegisterUrlRequestBody>(), It.IsAny<string>()))
-             .Returns(new RegisteredUrl() { LongUrl = "www.google.com", ShortUrl = "AStzHGv", AccountID = "12345" });
+             .Returns(new RegisteredUrl() { LongUrl = "www.google.com", ShortUrl = "AStzHGv", AccountID = "54321" });
 
-            _controller.ModelState.AddModelError("Url", "invalid data");
             RegisterUrlRequestBody request = new RegisterUrlRequestBody() { url = "www.google.com", redirectType = 300 }; //krivi redirectType
 
             var actionResult = _controller.RegisterUrl(request);
@@ -58,26 +78,35 @@ namespace UrlShortener.Tests.Controllers
 
         }
 
-        private void InitializeUnauthorizedController() {
+        private void InitializeControllerWithUnauthorizedContext() {
             var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
             var context = new DefaultHttpContext();
-            var claims = new[] { new Claim(ClaimTypes.NameIdentifier, "12345")};
+            var claims = new[] { new Claim(ClaimTypes.Name, "fake")}; //fake claim, treba biti type NameIdentifier
             var identity = new ClaimsIdentity(claims, "Basic");
             context.User = new GenericPrincipal(identity, new[] { "fake", "roles" });
 
             ControllerContext controllerContext = new ControllerContext();
             controllerContext.HttpContext = context;
 
-            /*var fakeTenantId = "abcd";
-            context.Request.Headers["User"] = fakeTenantId;*/
             mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(context);
-            _controller = new RegisterController(_mockService.Object, mockHttpContextAccessor.Object);
+            _controller = new UrlController(_mockService.Object, mockHttpContextAccessor.Object);
             _controller.ControllerContext = controllerContext;
         }
 
-        private void getAuthorizedControllerContext()
+        private void InitializeControllerWithAuthorizedContext()
         {
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var context = new DefaultHttpContext();
+            var claims = new[] { new Claim(ClaimTypes.NameIdentifier, "12345") }; 
+            var identity = new ClaimsIdentity(claims, "Basic");
+            context.User = new GenericPrincipal(identity, new[] { "fake", "roles" });
 
+            ControllerContext controllerContext = new ControllerContext();
+            controllerContext.HttpContext = context;
+
+            mockHttpContextAccessor.Setup(_ => _.HttpContext).Returns(context);
+            _controller = new UrlController(_mockService.Object, mockHttpContextAccessor.Object);
+            _controller.ControllerContext = controllerContext;
         }
     }
 }
